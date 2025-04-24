223fc0bec9f8e2cea8054b074da8b19359929067
vlecabr <vitartasleca@gmail.com>
2025-04-24 15:37:08 -0300

    trocado favicon e adicionado fotos de show

--- src/App.jsx
+++ src/App.jsx
@@ -75,8 +75,8 @@ export default function Home() {
         <SectionContent as={motion.div} initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.8 }}>
           <Title>Galeria de Fotos e Vídeos</Title>
           <Grid>
-            <Image src="/show1.jpg" alt="Show 1" />
-            <Image src="/show2.jpg" alt="Show 2" />
+            <Image src="/img/show1.jpg" alt="Show 1" />
+            <Image src="/img/show2.jpeg" alt="Show 2" />
             <Video src="https://www.youtube.com/embed/ScMzIvxBSi4" allowFullScreen title="YouTube Video" />
             <Video src="https://www.youtube.com/embed/ScMzIvxBSi4" allowFullScreen title="Outro Video" />
           </Grid>
