dfd453af1ea662d595db57d8ed16fda8c72100cb
vlecabr <vitartasleca@gmail.com>
2025-04-25 09:25:35 -0300

    img show1 updated

--- src/Components/Gallery/index.jsx
+++ src/Components/Gallery/index.jsx
@@ -22,7 +22,7 @@ export default function Gallery() {
       >
         <Title>Galeria de Fotos e Vídeos</Title>
         <Grid>
-          <Image src="/img/show1.jpg" alt="Show 1" />
+          <Image src="/img/show1.JPG" alt="Show 1" />
           <Image src="/img/show2.jpeg" alt="Show 2" />
           <Video src="https://www.youtube.com/embed/ScMzIvxBSi4" allowFullScreen title="YouTube Video" />
           <Video src="https://www.youtube.com/embed/ScMzIvxBSi4" allowFullScreen title="Outro Video" />
