9b23b77c697763114acc90abb4e775f59d228a3a
vlecabr <vitartasleca@gmail.com>
2025-04-25 14:19:37 -0300

    adicionado na exibição o footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Contact from './Components/Contact';
 import FloatingButton from './Components/Button';
 import { GlobalStyle } from './app.styles';
 import { Analytics } from "@vercel/analytics/react"
+import Footer from './Components/Footer';
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       <Gallery />
       <Events />
       <Contact />
+      <Footer />
     </>
   );
 }
