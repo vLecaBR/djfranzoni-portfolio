be7413531da6e3f514c320662fb0430e21cfc28f
vlecabr <vitartasleca@gmail.com>
2025-04-25 11:28:18 -0300

    testando analytics

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Events from './Components/Events';
 import Contact from './Components/Contact';
 import FloatingButton from './Components/Button';
 import { GlobalStyle } from './app.styles';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <>
+      <Analytics />
       <GlobalStyle />
       <FloatingButton />
       <Hero />
