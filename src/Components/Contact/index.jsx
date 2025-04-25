e85ca50128f83f006717e42db0f0ba0af1f8c74e
vlecabr <vitartasleca@gmail.com>
2025-04-25 14:51:13 -0300

    adicionando link no nome da duda

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -21,7 +21,7 @@ export default function Contact() {
         <Title>Contato / Redes Sociais</Title>
         <ContactText>
           Orçamentos: <br />
-          (16)99741-1851 (Duda)<br />
+          (16)99741-1851 <a href= "wa.me/message/TVVQBSUNBZPKA1" target="_blank" rel="noopener noreferrer">(Duda)</a><br />
           (16)99167-2353 (Mile)<br /><br />
           Siga nas redes sociais:<br />
           <a href="https://www.instagram.com/franzoni.dj/" target="_blank" rel="noopener noreferrer"> Instagram </a> |
