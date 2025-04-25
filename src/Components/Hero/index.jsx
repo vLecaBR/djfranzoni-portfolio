20a048728b0cf6e01b96b55a95a7bd0cebef241c
vlecabr <vitartasleca@gmail.com>
2025-04-25 09:22:30 -0300

    hero atualizado

--- src/Components/Hero/index.jsx
+++ src/Components/Hero/index.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
         transition={{ duration: 1 }}
       >
         <LogoImage src="/img/logo1.png" alt="DJ Franzoni" />
-        <HeroText>A mais mais de rp city</HeroText>
+        <HeroText>Eai Franzoni, solta aquela porra!</HeroText>
       </SectionContent>
     </HeroSection>
   );
