106e77ba4081e2b86a4cb80070e07e7fed5d003c
vlecabr <vitartasleca@gmail.com>
2025-08-14 15:58:38 -0300

    about atualizado!

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -21,10 +21,10 @@ export default function About() {
         <Title>Sobre a DJ</Title>
         <Text>
         Franzoni vem se destacando de forma cada vez mais intensa na cena do funk, consolidando seu nome como uma das artistas mais criativas e inovadoras da atualidade. <br></br>
-        Com produções autorais que carregam sua identidade única e uma habilidade incomparável de criar sets que hipnotizam a pista, ele conquista não apenas espaço, mas também respeito dentro do meio. <br></br>
+        Com produções autorais que carregam sua identidade única e uma habilidade incomparável de criar sets que hipnotizam a pista, ela conquista não apenas espaço, mas também respeito dentro do meio. <br></br>
         Sua curadoria musical vai muito além do comum: é uma mistura precisa entre originalidade, sofisticação e feeling, resultando em apresentações que surpreendem, envolvem e fazem o público viver uma verdadeira experiência sonora. 
         Cada performance é pensada para contar uma história, transportar quem ouve para um universo próprio e manter a energia lá em cima do início ao fim. <br></br>
-        Franzoni não apenas toca ele cria momentos que ficam na memória.
+        Franzoni não apenas toca ela cria momentos que ficam na memória.
         </Text>
       </SectionContent>
     </AboutSection>
