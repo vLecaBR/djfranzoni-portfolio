5010b9584470720917eaca18872e45b773eb6bbb
vlecabr <vitartasleca@gmail.com>
2026-02-25 11:28:49 -0300

    atualizado footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { FooterWrapper, FooterColumn, FooterText, SocialLinks } from './Footer.styles';
-import { FaInstagram } from 'react-icons/fa';
 
 export default function Footer() {
   return (
@@ -12,21 +11,12 @@ export default function Footer() {
           target="_blank"
           rel="noopener noreferrer"
           style={{ textDecoration: 'none', color: 'inherit' }}
-          >Victor Leça</a> | No Filter Creative. Todos os direitos reservados.
+          >Victor Leça</a>. Todos os direitos reservados.
         </FooterText>
       </FooterColumn>
 
       <FooterColumn>
         <SocialLinks>
-          <a
-            href="https://www.instagram.com/nofilter_creative/"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Instagram No Filter Creative"
-          >
-            <FaInstagram />
-            <span className="sr-only"></span>
-          </a>
         </SocialLinks>
       </FooterColumn>
     </FooterWrapper>
